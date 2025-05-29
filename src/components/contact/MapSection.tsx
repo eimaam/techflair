9ad95d1998edec9e9072426e55c07f7a2af45a69
eimaam <imamddahir@gmail.com>
2025-05-29 21:50:51 +0100

    refactor: remove placeholder text from MapSection for a cleaner UI

--- src/components/contact/MapSection.tsx
+++ src/components/contact/MapSection.tsx
@@ -8,7 +8,6 @@ const MapSection: React.FC = () => {
         {/* TODO: to add/embed map from google maps  */}
         <div className="w-full h-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center">
           <div className="text-center">
-            <p className="text-gray-600 dark:text-gray-300 mb-2">Interactive map would be here in production</p>
             <p className="font-medium text-gray-900 dark:text-white">MZ Aminu Street, CRD, FHA, Lugbe, FCT, Abuja, Nigeria</p>
           </div>
         </div>
